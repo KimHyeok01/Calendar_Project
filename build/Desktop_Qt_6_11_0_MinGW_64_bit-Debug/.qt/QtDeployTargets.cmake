55a565b137cc6fe7d2e2770d9d37484ebeb2d0cb
KimHyeok01 <163241850+KimHyeok01@users.noreply.github.com>
2026-04-20 12:07:03 +0900

    Delete build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Calendar_FILE C:/VEDA/qt-workspace/exercise/Calendar_Project/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/Calendar.exe)
-set(__QT_DEPLOY_TARGET_Calendar_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Calendar_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
